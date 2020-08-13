ce1c6b2cbc005e1c1ab3018752ac4c0fe2154d4c
Edgar <1452192394@qq.com>
2020-08-14 03:14:46 +0800

    commit 6

--- app/src/main/java/com/edgar/yurihome/HttpUtil.java
+++ app/src/main/java/com/edgar/yurihome/HttpUtil.java
@@ -5,6 +5,12 @@ import okhttp3.Request;
 
 public class HttpUtil {
 
+    public static final int REQUEST_JSON_SUCCESS = 100;
+    public static final int REQUEST_JSON_FAILED = 101;
+    public static final int PARSE_JSON_DATA_ERROR = 102;
+    public static final String MESSAGE_NETWORK_ERROR = "Network Error!";
+    public static final String MESSAGE_JSON_ERROR = "Json Data Error!";
+
     public static void sendRequestWithOkhttp(String urlString, okhttp3.Callback callback) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
--- app/src/main/java/com/edgar/yurihome/MainListFragment.java
+++ app/src/main/java/com/edgar/yurihome/MainListFragment.java
@@ -15,7 +15,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +40,8 @@ public class MainListFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private static final int MSG_LOAD_NEXT_PAGE = 1000;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -36,8 +49,14 @@ public class MainListFragment extends Fragment {
     private RecyclerView recyclerView;
     private MainListAdapter adapter;
     private SwipeRefreshLayout srlMain;
+    private FloatingActionButton fab;
     private ArrayList<ComicItem> comicItems = new ArrayList<>();
     private Handler mHandler;
+
+    private boolean isLoading = false;
+    private boolean isFinalPage = false;
+
+    private int typeCode = 3243, regionCode = 2304, groupCode = 0, statusCode = 0, sortCode = 1, page = 0;
 
     public MainListFragment() {
         // Required empty public constructor
@@ -76,22 +95,67 @@ public class MainListFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main_list, container, false);
         srlMain = rootView.findViewById(R.id.srl_main);
+        fab = rootView.findViewById(R.id.fab_top);
+        fab.hide();
         recyclerView = rootView.findViewById(R.id.rv_main);
         GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 3);
         recyclerView.setLayoutManager(layoutManager);
         adapter = new MainListAdapter(getContext(), comicItems);
         recyclerView.setAdapter(adapter);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.scrollToPosition(0);
+            }
+        });
+
         mHandler = new Handler(Looper.getMainLooper()) {
             @Override
             public void handleMessage(@NonNull Message msg) {
                 super.handleMessage(msg);
+                switch (msg.what) {
+                    case HttpUtil.REQUEST_JSON_SUCCESS:
+                        if (comicItems.isEmpty()) {
+                            isFinalPage = true;
+                            Snackbar.make(recyclerView, R.string.string_no_more_data, Snackbar.LENGTH_SHORT).show();
+                        } else {
+                            if (srlMain.isRefreshing()) {
+                                adapter.setComicItems(comicItems);
+                            } else {
+                                adapter.appendComicItems(comicItems);
+                            }
+                        }
+                        break;
+
+                    case HttpUtil.REQUEST_JSON_FAILED:
+                        Snackbar.make(recyclerView, HttpUtil.MESSAGE_NETWORK_ERROR, Snackbar.LENGTH_SHORT).show();
+                        if (page >= 1) {
+                            page -= 1;
+                        }
+                        break;
+
+                    case HttpUtil.PARSE_JSON_DATA_ERROR:
+                        Snackbar.make(recyclerView, HttpUtil.MESSAGE_JSON_ERROR, Snackbar.LENGTH_SHORT).show();
+                        if (page >= 1) {
+                            page -= 1;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                isLoading = false;
+                srlMain.setRefreshing(false);
             }
         };
 
         srlMain.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                page = 0;
+                isFinalPage = false;
+                loadPageData(mHandler, typeCode, regionCode, groupCode, statusCode, sortCode, page);
 
             }
         });
@@ -100,6 +164,17 @@ public class MainListFragment extends Fragment {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
+                GridLayoutManager layoutManager1 = (GridLayoutManager) recyclerView.getLayoutManager();
+                if (layoutManager1 != null && layoutManager1.findFirstCompletelyVisibleItemPosition() <= 9 && fab.isShown()) {
+                    fab.hide();
+                }
+                if (layoutManager1 != null && layoutManager1.findLastCompletelyVisibleItemPosition() > 14 && !fab.isShown()) {
+                    fab.show();
+                }
+                if (isSlideToBottom(recyclerView, isLoading)) {
+                    page += 1;
+                    loadPageData(mHandler, typeCode, regionCode, groupCode, statusCode, sortCode, page);
+                }
             }
         });
 
@@ -110,5 +185,50 @@ public class MainListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        loadPageData(mHandler, typeCode, regionCode, groupCode, statusCode, sortCode, page);
+    }
+
+    private boolean isSlideToBottom(RecyclerView rvArg, boolean isLoadingArg) {
+        return !isLoadingArg && rvArg != null &&
+                rvArg.computeVerticalScrollExtent() + rvArg.computeVerticalScrollOffset() >=
+                        rvArg.computeVerticalScrollRange();
+    }
+
+    private void loadPageData(final Handler handler, int type, int region, int group, int status, int sort, int pageNum) {
+        if (isFinalPage) return;
+        isLoading = true;
+        String urlString = Config.getComicsUrlByFilter(type, region, group, status, sort, pageNum);
+        HttpUtil.sendRequestWithOkhttp(urlString, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Message message = Message.obtain();
+                message.what = HttpUtil.REQUEST_JSON_FAILED;
+                handler.sendMessage(message);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Message message = Message.obtain();
+                message.what = parseJsonString(response);
+                handler.sendMessage(message);
+            }
+        });
+    }
+
+    private int parseJsonString(Response response) {
+        int result = HttpUtil.PARSE_JSON_DATA_ERROR;
+        try {
+            if (response.body() == null) return result;
+            String jsonString = response.body().string();
+            Gson gson = new Gson();
+            Type type = new TypeToken<ArrayList<ComicItem>>() {
+            }.getType();
+            ArrayList<ComicItem> items = gson.fromJson(jsonString, type);
+            comicItems = new ArrayList<>(items);
+            result = HttpUtil.REQUEST_JSON_SUCCESS;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
